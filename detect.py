85839b1bc9f31a63d0962138b01f03b9d5eb659e
aiya714 <87637190+aiya714@users.noreply.github.com>
2022-11-30 13:20:16 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -154,8 +154,6 @@ def run(
                 add_iou_dif = []
                 for index in range(len(bb)-1) :
                     a, b = det[bb[index]], det[bb[index+1]]
-                    print([i for i in a ])
-                    print()
                     area_a = (a[2] - a[0]) * (a[3] - a[1])
                     area_b = (b[2] - b[0]) * (b[3] - b[1])
                     w, h = min(a[2],b[2]) - max(a[0],b[0]), min(a[3],b[3]) - max(a[1],b[1])
